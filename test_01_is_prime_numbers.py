4e5d0528e23e1800df396642b4319b5770e715a0
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2026-02-17 11:23:21 -0700

    path corrected

--- test_01_is_prime_numbers.py
+++ test_01_is_prime_numbers.py
@@ -15,7 +15,7 @@ from io import StringIO
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from src.main import is_prime_numbers
 
 
